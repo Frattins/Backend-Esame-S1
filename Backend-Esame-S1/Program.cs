c801662ec40f489ed83f036763633500e9e978d3
Frattins <davide.frattini1@gmail.com>
2024-06-21 13:08:30 +0200

    Aggiungere i file di progetto.

---
+++ Backend-Esame-S1/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace Backend_Esame_S1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Inserisci i dati del contribuente:");
+
+            Console.Write("Nome:  ");
+            string nome = Console.ReadLine();
+
+            Console.Write("Cognome: ");
+            string cognome = Console.ReadLine();
+
+            Console.Write("Data di Nascita (gg/mm/aaaa):");
+            DateTime dataNascita = DateTime.Parse(Console.ReadLine());
+
+            Console.Write("Codice Fiscale:");
+            string codiceFiscale = Console.ReadLine();
+
+            Console.Write("Sesso (M/F):");
+            char sesso = char.Parse(Console.ReadLine());
+
+            Console.Write("Comune di Residenza: ");
+            string comuneResidenza = Console.ReadLine();
+
+            Console.Write("Reddito Annuale: ");
+            decimal redditoAnnuale = decimal.Parse(Console.ReadLine());
+
+            contribuente contribuente = new contribuente(nome, cognome, dataNascita, codiceFiscale, sesso, comuneResidenza, redditoAnnuale);
+
+            decimal imposta = contribuente.CalcolaImposta();
+
+            Console.WriteLine("===============================================");
+            Console.WriteLine("CALCOLO DELL'IMPOSTA DA VERSARE:");
+            Console.WriteLine($"Contribuente: {contribuente.Nome} {contribuente.Cognome},");
+            Console.WriteLine($"nato il {contribuente.DataNascita.ToShortDateString()} ({contribuente.Sesso}),");
+            Console.WriteLine($"residente in {contribuente.ComuneResidenza},");
+            Console.WriteLine($"codice fiscale: {contribuente.CodiceFiscale}");
+            Console.WriteLine($"Reddito dichiarato: {contribuente.RedditoAnnuale:C}");
+            Console.WriteLine($"IMPOSTA DA VERSARE: {imposta:C}");
+        }
+    }
+}
---
+++ Backend-Esame-S1/contribuente.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Backend_Esame_S1
+{
+    internal class contribuente
+    {
+
+    public string Nome { get; set; }
+        public string Cognome { get; set; }
+        public DateTime DataNascita { get; set; }
+        public string CodiceFiscale { get; set; }
+        public char Sesso { get; set; }
+        public string ComuneResidenza { get; set; }
+        public decimal RedditoAnnuale { get; set; }
+
+        public contribuente(string nome, string cognome, DateTime dataNascita, string codiceFiscale, char sesso, string comuneResidenza, decimal redditoAnnuale)
+        {
+            Nome = nome;
+            Cognome = cognome;
+            DataNascita = dataNascita;
+            CodiceFiscale = codiceFiscale;
+            Sesso = sesso;
+            ComuneResidenza = comuneResidenza;
+            RedditoAnnuale = redditoAnnuale;
+        }
+
+        public decimal CalcolaImposta()
+        {
+            decimal imposta = 0;
+
+            if (RedditoAnnuale <= 15000)
+            {
+                imposta = RedditoAnnuale * 0.23m;
+            }
+            else if (RedditoAnnuale <= 28000)
+            {
+                imposta = 15000 * 0.23m + (RedditoAnnuale - 15000) * 0.27m;
+            }
+            else if (RedditoAnnuale <= 55000)
+            {
+                imposta = 15000 * 0.23m + 13000 * 0.27m + (RedditoAnnuale - 28000) * 0.38m;
+            }
+            else if (RedditoAnnuale <= 75000)
+            {
+                imposta = 15000 * 0.23m + 13000 * 0.27m + 27000 * 0.38m + (RedditoAnnuale - 55000) * 0.41m;
+            }
+            else
+            {
+                imposta = 15000 * 0.23m + 13000 * 0.27m + 27000 * 0.38m + 20000 * 0.41m + (RedditoAnnuale - 75000) * 0.43m;
+            }
+
+            return imposta;
+        }
+    }
+}
+
